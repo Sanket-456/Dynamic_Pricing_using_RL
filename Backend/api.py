8765c936cc83929b19efdc26d19c23bb260ffcac
Sanket-456 <kalesanket518@gmail.com>
2026-04-05 12:30:26 +0530

    Added port requirements

--- Backend/api.py
+++ Backend/api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify ,request
 from flask_cors import CORS
 import numpy as np
@@ -100,4 +101,8 @@ def simulate_route():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Get the port Render assigned, or default to 5000 for local testing
+    port = int(os.environ.get("PORT", 5000))
+
+    # host="0.0.0.0" is the magic key that opens it to the internet
+    app.run(host="0.0.0.0", port=port)
